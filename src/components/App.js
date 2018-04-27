bc64a9ec6402553301ac73c262a4e2948559beb5
Tolik Dem <tolik.demianov@gmail.com>
2018-04-27 20:13:59 +0300

    вариант с yandex-map-react модулем

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Map, Marker, MarkerLayout } from 'yandex-map-react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return(
-      <div>Welcome to Route Editor</div>
+      <div>
+        <Map onAPIAvailable={() => console.log('API loaded')} center={[55.754734, 37.583314]} zoom={5}>
+          <Marker lat={55.75} lon={37.9} />
+        </Map>
+      </div>
     )
   }
 }
